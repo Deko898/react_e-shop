0d22e983cb10ee75bf582cb2863ed4748741e6d1
Dejan Obradovikj <dejan.obradovikj@seavus.com>
2019-04-28 22:58:30 +0200

    push to heroku

--- client/src/components/Header_footer/Header/index.js
+++ client/src/components/Header_footer/Header/index.js
@@ -117,7 +117,7 @@ class Header extends Component {
                 <div className="container">
                     <div className="left">
                         <div className="logo">
-                            E-Shope
+                            E-Shop
                         </div>
                     </div>
                     <div className="right">
